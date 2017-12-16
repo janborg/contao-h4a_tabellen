05b6ef516ee49bcea8f56982ff390e43f0c2d66b
Jan Lünborg <janluenborg@gmail.com>
2017-12-16 11:19:42 +0100

    start as Bundle

---
+++ ContaoManagerPlugin.php
@@ -0,0 +1,19 @@
+<?php
+namespace Janborg\H4aTabellen;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBundles(ParserInterface $parser)
+	{
+		return [
+			BundleConfig::create(JanborgH4aTabellenBundle::class)
+				->setLoadAfter([ContaoCoreBundle::class])
+		];
+	}
+}
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+*
+ */
+
+$GLOBALS['TL_CTE']['handball']['h4a_tabelle']= 'Janborg//H4aTabellen//Elements//ContentH4aTabelle';
+$GLOBALS['TL_CTE']['handball']['h4a_spiele']= 'Janborg//H4aTabellen//Elements//ContentH4aSpiele';
