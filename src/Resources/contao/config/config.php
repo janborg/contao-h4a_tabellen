72b6290a48c348d8318d96bfb2b9e78dd1b768d4
Jan Lünborg <janluenborg@gmail.com>
2017-12-17 20:43:31 +0100

    Elements Config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,7 +3,7 @@
 /*H4aTabellen
 *filesource
  */
-use Janborg\H4aTabellen\Element;
+use Janborg\H4aTabellen\Elements;
 
-$GLOBALS['TL_CTE']['handball']['h4a_spiele']= ContentH4aSpiele::class;
-$GLOBALS['TL_CTE']['handball']['h4a_tabelle']= ContentH4aTabelle::class;
+$GLOBALS['TL_CTE']['handball']['h4a_spiele']    = ContentH4aSpiele::class;
+$GLOBALS['TL_CTE']['handball']['h4a_tabelle']   = ContentH4aTabelle::class;
