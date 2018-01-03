a1425cc4d5ff0733acd50c57c041dbabefb53ca6
Jan Lünborg <janluenborg@gmail.com>
2018-01-03 20:07:40 +0100

    Cronjob added

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,3 +9,6 @@ $GLOBALS['TL_CTE']['handball']['h4a_tabelle'] = Janborg\H4aTabellen\Elements\Con
 
 
 $GLOBALS['BE_MOD']['content']['calendar']['update_events'] = ['Janborg\H4aTabellen\Cron\H4aEventAutomator', 'updateEvents'];
+
+// Register Cron job
+$GLOBALS['TL_CRON']['daily'][] = ['Janborg\H4aTabellen\Cron\H4aEventAutomator', 'updateEvents'];
