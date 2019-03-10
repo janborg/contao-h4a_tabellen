814d97fb7d4996c084098d9a08222cd790ef96e0
Jan Lünborg <janluenborg@gmail.com>
2019-03-10 19:48:30 +0100

    php-cs-fixer

--- tests/JanborgH4aTabellenBundleTest.php
+++ tests/JanborgH4aTabellenBundleTest.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of contao-h4a_tabellen.
+ * (c) Jan Lünborg
+ * @license LGPL-3.0-or-later
+ */
+
 namespace Janborg\H4aTabellen\Tests;
 
 use Janborg\H4aTabellen\JanborgH4aTabellenBundle;
