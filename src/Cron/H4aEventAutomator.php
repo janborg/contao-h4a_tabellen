45cd222f7a990622a8c88726335ef2cb9629d471
Jan Lünborg <janborg@fedoraborg.fritz.box>
2020-02-24 18:42:53 +0100

    use \StringUtil::generateaAlias instead of standardize and restoreBAsicEntities

--- src/Cron/H4aEventAutomator.php
+++ src/Cron/H4aEventAutomator.php
@@ -126,7 +126,7 @@ class H4aEventAutomator extends Backend
                     $objEvent->pid = $objCalendar->id;
                     $objEvent->timestamp = time();
                     $objEvent->title = $arrSpiel['gClassSname'].': '.$arrSpiel['gHomeTeam'].' - '.$arrSpiel['gGuestTeam'];
-                    $objEvent->alias = standardize(\StringUtil::restoreBasicEntities($arrSpiel['gClassSname'].'_'.$arrSpiel['gHomeTeam'].'_'.$arrSpiel['gGuestTeam'].'_'.$arrSpiel['gNo']));
+                    $objEvent->alias = \StringUtil::generateAlias($arrSpiel['gClassSname'].'_'.$arrSpiel['gHomeTeam'].'_'.$arrSpiel['gGuestTeam'].'_'.$arrSpiel['gNo']);
                     $objEvent->gGameID = $arrSpiel['gID'];
                     $objEvent->gGameNo = $arrSpiel['gNo'];
                     $objEvent->gClassName = $arrSpiel['gClassSname'];
