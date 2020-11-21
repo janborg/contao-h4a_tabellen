848e76f9f95c4a19325a424e154200715d182102
Jan <jan-github@luenborg.eu>
2020-11-21 18:05:19 +0100

    Fehler in der Logik

--- src/Cron/H4aEventAutomator.php
+++ src/Cron/H4aEventAutomator.php
@@ -206,7 +206,7 @@ class H4aEventAutomator extends Backend
             return;
         }
         foreach ($objEvents as $objEvent) {
-            if ($objEvent->startTime > time() and date("H:i",$objEvent->startTime) != "00:00") {
+            if ($objEvent->startTime > time() or date("H:i",$objEvent->startTime) == "00:00") {
                 continue;
             }
 
