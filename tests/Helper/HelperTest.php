97f3368bb5fbbfef139c0d01a26ef29ca5a8fa3f
janborg <jan-github@luenborg.eu>
2021-08-05 15:28:59 +0200

    add Test for Spielplan Json

--- tests/Helper/HelperTest.php
+++ tests/Helper/HelperTest.php
@@ -39,4 +39,34 @@ class HelperTest extends TestCase
 
         $this->assertSame($typePath, $result[0]['lvTypePathStr']);
     }
+
+    public function testJsonSpielplanHasCorrectDataFields(): void
+    {
+        $spielplan = Helper::getJsonSpielplan('551206');
+        $spiel = $spielplan['dataList']['0'];
+
+        $this->assertArrayHasKey('lvTypePathStr', $spielplan);
+        $this->assertArrayHasKey('lvIDPathStr', $spielplan);
+        $this->assertArrayHasKey('lvTypeLabelStr', $spielplan);
+        $this->assertArrayHasKey('dataList', $spielplan);
+        
+        $this->assertArrayHasKey('gID', $spiel);
+        $this->assertArrayHasKey('gNo', $spiel);
+        $this->assertArrayHasKey('gClassID', $spiel);
+        $this->assertArrayHasKey('gClassSname', $spiel);
+        $this->assertArrayHasKey('gDate', $spiel);
+        $this->assertArrayHasKey('gTime', $spiel);
+        $this->assertArrayHasKey('gGymnasiumID', $spiel);
+        $this->assertArrayHasKey('gGymnasiumNo', $spiel);
+        $this->assertArrayHasKey('gGymnasiumName', $spiel);
+        $this->assertArrayHasKey('gGymnasiumPostal', $spiel);
+        $this->assertArrayHasKey('gGymnasiumTown', $spiel);
+        $this->assertArrayHasKey('gGymnasiumStreet', $spiel);
+        $this->assertArrayHasKey('gHomeTeam', $spiel);
+        $this->assertArrayHasKey('gGuestTeam', $spiel);
+        $this->assertArrayHasKey('gHomeGoals', $spiel);
+        $this->assertArrayHasKey('gGuestGoals', $spiel);
+        $this->assertArrayHasKey('gHomeGoals_1', $spiel);
+        $this->assertArrayHasKey('gGuestGoals_1', $spiel);
+    }
 }
