48c65b69fe35a8add3b7d3b5d3098ac279837ccc
janborg <jan-github@luenborg.eu>
2021-08-10 16:38:20 +0200

    Ergänzung der Elementbeschreibungen

--- src/Resources/contao/languages/de/tl_h4a.php
+++ src/Resources/contao/languages/de/tl_h4a.php
@@ -71,14 +71,24 @@ $GLOBALS['TL_LANG']['tl_calendar_events']['gHomeGoals_1']['1'] = 'Wird automatis
 $GLOBALS['TL_LANG']['tl_calendar_events']['gGuestGoals_1']['0'] = 'Gasttore Halbzeit';
 $GLOBALS['TL_LANG']['tl_calendar_events']['gGuestGoals_1']['1'] = 'Wird automatisch beim Update über handball4all gesetzt';
 
-
  /*
  * tl_content
  */
 
 $GLOBALS['TL_LANG']['CTE']['handball'] = 'Handball4all';
+
 $GLOBALS['TL_LANG']['CTE']['h4a_tabelle']['0'] = 'h4a-Tabelle';
+$GLOBALS['TL_LANG']['CTE']['h4a_tabelle']['1'] = 'Erzeugt die aktuelle Tabelle für die angegebene Liga';
+
 $GLOBALS['TL_LANG']['CTE']['h4a_spiele']['0'] = 'h4a-Spielplan';
+$GLOBALS['TL_LANG']['CTE']['h4a_spiele']['1'] = 'Erzeugt den Spielplan für das angegebene Team';
+
+$GLOBALS['TL_LANG']['CTE']['h4a_ligaspiele']['0'] = 'h4a-Liga-Spielplan';
+$GLOBALS['TL_LANG']['CTE']['h4a_ligaspiele']['1'] = 'Erzeugt den Spielplan für die angegebene Liga';
+
+$GLOBALS['TL_LANG']['CTE']['h4a_aktuellespiele']['0'] = 'h4a-Aktuelle Spiele';
+$GLOBALS['TL_LANG']['CTE']['h4a_aktuellespiele']['1'] = 'Erzeugt die akteulle Spielüberischt des Vereins';
+
 
 $GLOBALS['TL_LANG']['tl_content']['h4a_liga_ID']['0'] = 'Liga ID';
 $GLOBALS['TL_LANG']['tl_content']['h4a_liga_ID']['1'] = 'Bitte geben Sie die 5-stellige ID der Liga (www.handball4all.de) ein';
