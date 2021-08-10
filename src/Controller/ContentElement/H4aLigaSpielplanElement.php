4869660fb3d4ead318b3719652ba91a369609758
janborg <jan-github@luenborg.eu>
2021-08-10 13:42:06 +0200

    add Controller for LigaSpielplanElement

---
+++ src/Controller/ContentElement/H4aLigaSpielplanElement.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of contao-h4a_tabellen.
+ * (c) Jan Lünborg
+ * @license MIT
+ */
+
+namespace Janborg\H4aTabellen\Controller\ContentElement;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\CoreBundle\ServiceAnnotation\ContentElement;
+use Contao\Template;
+use Janborg\H4aTabellen\Helper\Helper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @ContentElement("h4a_ligaspiele",
+ *   category="texts",
+ *   template="ce_h4a_ligaspiele",
+ * )
+ */
+class H4aLigaSpielplanElement extends AbstractContentElementController
+{
+    protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
+    {
+        $arrResult = Helper::getJsonLigaSpielplan($model->h4a_liga_ID);
+        $lastUpdate = time();
+
+        $template->spiele = $arrResult['dataList'];
+        $template->class = 'ce_h4a_ligaspiele';
+        $template->myTeam = $model->my_team_name;
+        $template->lastUpdate = $lastUpdate;
+
+        return $template->getResponse();
+    }
+}
