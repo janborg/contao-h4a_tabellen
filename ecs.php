9f9632b8cd20c0624ba2a2caabe0582e798a90d4
janborg <jan-github@luenborg.eu>
2021-09-15 16:44:27 +0200

    add contao easy coding standards

---
+++ ecs.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__.'/vendor/contao/easy-coding-standard/config/set/contao.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::LINE_ENDING, "\n");
+
+    $services = $containerConfigurator->services();
+    $services
+        ->set(HeaderCommentFixer::class)
+        ->call('configure', [[
+            'header' => "This file is part of contao-h4a_tabellen.\n\n(c) Jan Lünborg\n\n@license MIT",
+        ]])
+    ;
+};
