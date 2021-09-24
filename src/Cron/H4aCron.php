55b5964f1905ca070208da41708ca85356a43cbc
janborg <jan-github@luenborg.eu>
2021-09-24 12:40:35 +0200

    add Cron updateEvents()

--- src/Cron/H4aCron.php
+++ src/Cron/H4aCron.php
@@ -3,9 +3,11 @@
 namespace Janborg\H4aTabellen\Cron;
 
 use Contao\CalendarEventsModel;
+use Contao\CalendarModel;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Monolog\ContaoContext;
 use Contao\System;
+use Janborg\H4aTabellen\H4aEventautomator\H4aEventAutomator;
 use Janborg\H4aTabellen\Helper\Helper;
 use Psr\Log\LogLevel;
 
@@ -21,7 +23,30 @@ class H4aCron
         $this->framework = $framework;
         $this->framework->initialize();
     }
+    
+    public function updateEvents(): void
+    {
+        $objCalendars = CalendarModel::findby(
+            ['tl_calendar.h4a_imported=?', 'tl_calendar.h4a_ignore !=?'],
+            ['1', '1']
+        );
+        
+        foreach ($objCalendars as $objCalendar) {
 
+            $h4aeventautomator = new H4aEventAutomator;
+            
+            $h4aeventautomator->syncCalendars($objCalendar);
+
+            System::getContainer()
+                        ->get('monolog.logger.contao')
+                        ->log(LogLevel::INFO, 'Update des Kalenders "'.$objCalendar->title.'" (ID: '.$objCalendar->id.') über Handball4all durchgeführt.', ['contao' => new ContaoContext(__CLASS__.'::'.__FUNCTION__, TL_CRON)]);
+        }
+    }
+
+    public function updateResults(): void
+    {
+        // Do something …
+    }
     public function updateReports(): void
     {
         $objEvents = CalendarEventsModel::findby(
