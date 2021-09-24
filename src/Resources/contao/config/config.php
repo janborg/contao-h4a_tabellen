555620760cb6f3d079e2ef6189378c363336e1e9
janborg <jan-github@luenborg.eu>
2021-09-24 12:31:14 +0200

    remove "old way" of registering cronjobs

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -14,10 +14,5 @@ $GLOBALS['BE_MOD']['content']['calendar']['update_events'] = ['Janborg\H4aTabell
 $GLOBALS['BE_MOD']['content']['calendar']['update_calendar'] = ['Janborg\H4aTabellen\H4aEventAutomator\H4aEventAutomator', 'updateArchive'];
 $GLOBALS['BE_MOD']['content']['calendar']['update_results'] = ['Janborg\H4aTabellen\H4aEventAutomator\H4aEventAutomator', 'updateResults'];
 
-// Register Cron job
-$GLOBALS['TL_CRON']['daily'][] = ['Janborg\H4aTabellen\Cron\H4aEventAutomator', 'updateEvents'];
-$GLOBALS['TL_CRON']['hourly'][] = ['Janborg\H4aTabellen\Cron\H4aEventAutomator', 'updateResults'];
-$GLOBALS['TL_CRON']['hourly'][] = ['Janborg\H4aTabellen\Cron\H4aEventAutomator', 'updateReportIDs'];
-
 // Register Models
 $GLOBALS['TL_MODELS']['tl_h4ajsondata'] = Janborg\H4aTabellen\Model\H4aJsonDataModel::class;
