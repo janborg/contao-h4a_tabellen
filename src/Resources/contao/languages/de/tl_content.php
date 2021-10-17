0e7f7133b9d3bb0cb4aa08b970e688f8dd51693e
janborg <jan-github@luenborg.eu>
2021-10-17 18:57:35 +0200

    add missing TL_LANG for h4a_verein_ID

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -22,7 +22,11 @@ $GLOBALS['TL_LANG']['tl_content']['h4a_liga_ID']['1'] = 'Bitte geben Sie die 5-s
 $GLOBALS['TL_LANG']['tl_content']['h4a_team_ID']['0'] = 'Team ID';
 $GLOBALS['TL_LANG']['tl_content']['h4a_team_ID']['1'] = 'Bitte geben Sie die 6-stellige ID des Teams (www.handball4all.de) ein';
 
+$GLOBALS['TL_LANG']['tl_content']['h4a_verein_ID']['0'] = 'Verein ID';
+$GLOBALS['TL_LANG']['tl_content']['h4a_verein_ID']['1'] = 'Bitte geben Sie die ID des Vereins (www.handball4all.de) ein';
+
 $GLOBALS['TL_LANG']['tl_content']['my_team_name']['0'] = 'Mein Team';
 $GLOBALS['TL_LANG']['tl_content']['my_team_name']['1'] = 'Genaue Bezeichnung der Mannschaft, die hervorgehoben werden soll';
+
 
 $GLOBALS['TL_LANG']['tl_content']['h4a_legend'] = 'Handball4all-Daten';
