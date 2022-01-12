c1d82e666ef197050d7f5b97e412a5b8e9f35067
janborg <jan-github@luenborg.eu>
2022-01-12 19:52:50 +0100

    standardfeld ClassID nutzen

--- src/Controller/ContentElement/H4aAktuelleSpieleElement.php
+++ src/Controller/ContentElement/H4aAktuelleSpieleElement.php
@@ -34,7 +34,6 @@ class H4aAktuelleSpieleElement extends AbstractContentElementController
         $lastUpdate = time();
 
         $template->spiele = $arrResult['dataList'];
-        $template->class = 'ce_h4a_aktuellespiele';
         $template->myTeam = $model->my_team_name;
         $template->lastUpdate = $lastUpdate;
 
