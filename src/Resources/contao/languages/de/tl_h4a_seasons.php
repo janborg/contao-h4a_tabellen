660d1858dedffdd471eaef8b8fda3c70a5ca6fe6
Janborg <jan-github@luenborg.eu>
2022-06-04 18:00:39 +0200

    add translations

---
+++ src/Resources/contao/languages/de/tl_h4a_seasons.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_h4a_seasons']['season']['0'] = 'Saison';
+$GLOBALS['TL_LANG']['tl_h4a_seasons']['season']['1'] = 'Die Bezeichnung der Saison, z.Bsp. "2019/2020"';
+
+$GLOBALS['TL_LANG']['tl_h4a_season']['is_current_season']['0'] = 'Aktuelle Saison';
+$GLOBALS['TL_LANG']['tl_h4a_season']['is_current_season']['1'] = 'Häkchen setzen,wenn dies die aktuelle Saison ist';
