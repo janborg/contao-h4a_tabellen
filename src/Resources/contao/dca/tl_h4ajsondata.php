3b4d7f8c5842bf96fdce8dae53be5255a4b1ce98
Janborg <jan-github@luenborg.eu>
2022-06-05 09:16:16 +0200

    php8 array fix

--- src/Resources/contao/dca/tl_h4ajsondata.php
+++ src/Resources/contao/dca/tl_h4ajsondata.php
@@ -51,7 +51,7 @@ $GLOBALS['TL_DCA']['tl_h4ajsondata'] =
                 'label' => &$GLOBALS['TL_LANG']['tl_h4ajsondata']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+				'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ],
             'show' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_h4ajsondata']['show'],
