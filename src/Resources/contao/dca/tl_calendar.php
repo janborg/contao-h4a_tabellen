0cfaa66dfb1ff788b886764bd4d09587e89280c9
Janborg <jan-github@luenborg.eu>
2022-10-21 06:49:58 +0200

    replace mcw by cgw

--- src/Resources/contao/dca/tl_calendar.php
+++ src/Resources/contao/dca/tl_calendar.php
@@ -10,10 +10,10 @@ declare(strict_types=1);
  * @license MIT
  */
 
-use Contao\Backend;
+
 use Contao\BackendUser;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
-use Contao\StringUtil;
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 
 /*
  * Global Operation(s)
@@ -48,15 +48,15 @@ $GLOBALS['TL_DCA']['tl_calendar']['list']['global_operations'] = array_merge(
 PaletteManipulator::create()
     ->addLegend('h4a_legend', 'title_legend', PaletteManipulator::POSITION_AFTER)
     ->addField('h4a_imported', 'h4a_legend', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('default', 'tl_calendar')
-;
+    ->applyToPalette('default', 'tl_calendar');
 
 /*
  * Add Selector(s)
  */
 
 $GLOBALS['TL_DCA']['tl_calendar']['palettes']['__selector__'] = array_merge(
-    ['h4a_imported',
+    [
+        'h4a_imported',
     ],
     $GLOBALS['TL_DCA']['tl_calendar']['palettes']['__selector__']
 );
@@ -66,7 +66,8 @@ $GLOBALS['TL_DCA']['tl_calendar']['palettes']['__selector__'] = array_merge(
  */
 
 $GLOBALS['TL_DCA']['tl_calendar']['subpalettes'] = array_merge(
-    ['h4a_imported' => 'h4a_seasons, h4aEvents_author, h4a_ignore',
+    [
+        'h4a_imported' => 'h4a_seasons, h4aEvents_author',
     ],
     $GLOBALS['TL_DCA']['tl_calendar']['subpalettes']
 );
@@ -84,63 +85,56 @@ $GLOBALS['TL_DCA']['tl_calendar']['fields'] = array_merge(
         'sql' => "char(1) NOT NULL default ''",
     ]],
     ['h4a_seasons' => [
-        'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_seasons'],
-        'exclude' => true,
-        'inputType' => 'multiColumnWizard',
-        'eval' => [
-            'style' => 'width: 680px;',
-            'dragAndDrop'  => true,
-            'buttons'      => [
-                'copy'   => false,
-                'up'     => true,
-                'down'   => true
+        'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_season'],
+        'exclude' => false,
+        'inputType' => 'group',
+        'palette' => ['h4a_season', 'h4a_team', 'h4a_liga', 'my_team_name'],
+        'fields' => [
+            'h4a_season' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_saison'],
+                'inputType' => 'select',
+                'foreignKey' => 'tl_h4a_seasons.season',
+                'relation' => ['type' => 'hasOne', 'load' => 'lazy'],
+                'eval' => [
+                    'mandatory' => true,
+                    'tl_class' => 'w50',
+                    'includeBlankOption' => true,
+                    'chosen' => true,
+                ],
             ],
-            'columnFields' => [
-                'h4a_saison' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_saison'],
-                    'inputType' => 'select',
-                    'foreignKey' => 'tl_h4a_seasons.season',
-                    'relation' => ['type'=>'hasOne', 'load'=>'lazy'],
-                    'eval' => [
-                        'mandatory' => true,
-                        'style' => 'width:150px;',
-                        'includeBlankOption' => true,
-                        'chosen' => true,
-                    ],
+            'h4a_team' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_team'],
+                'inputType' => 'text',
+                'eval' => [
+                    'mandatory' => true,
+                    'rgxp' => 'digit',
+                    'maxlength' => 6,
+                    'tl_class' => 'w50',
                 ],
-                'h4a_team' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_team'],
-                    'inputType' => 'text',
-                    'eval' => [
-                        'mandatory' => true,
-                        'rgxp' => 'digit',
-                        'maxlength' => 6,
-                        'style' => 'width:100px;',
-                    ],
+            ],
+            'h4a_liga' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_liga_ID'],
+                'inputType' => 'text',
+                'eval' => [
+                    'mandatory' => true,
+                    'rgxp' => 'digit',
+                    'maxlength' => 5,
+                    'tl_class' => 'w50',
                 ],
-                'h4a_liga' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4a_liga_ID'],
-                    'inputType' => 'text',
-                    'eval' => [
-                        'mandatory' => true,
-                        'rgxp' => 'digit',
-                        'maxlength' => 5,
-                        'style' => 'width:100px;',
-                    ],
-                ],
-                'my_team_name' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_calendar']['my_team_name'],
-                    'inputType' => 'text',
-                    'eval' => [
-                        'mandatory' => true,
-                        'maxlength' => 255,
-                        'style' => 'width:250px;',
-                    ],
+            ],
+            'my_team_name' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_calendar']['my_team_name'],
+                'inputType' => 'text',
+                'eval' => [
+                    'mandatory' => true,
+                    'maxlength' => 255,
+                    'tl_class' => 'w50',
                 ],
             ],
         ],
-        'sql' => 'blob NULL',
+        'sql' => "blob NULL",
     ]],
+    
     ['h4aEvents_author' => [
         'label' => &$GLOBALS['TL_LANG']['tl_calendar']['h4aEvents_author'],
         'default' => BackendUser::getInstance()->id,
