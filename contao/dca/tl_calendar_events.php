9a1c524e3858e632708d7095e7a531666f3987a7
Janborg <jan-github@luenborg.eu>
2024-04-05 17:59:43 +0200

    fix global_operations

--- contao/dca/tl_calendar_events.php
+++ contao/dca/tl_calendar_events.php
@@ -20,7 +20,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['list']['global_operations'] = array_me
         'href' => 'key=h4a_update_events',
         'icon' => 'bundles/janborgh4atabellen/update.svg',
     ]],
-    $GLOBALS['TL_DCA']['tl_calendar_events']['list']['global_operations']
+    $GLOBALS['TL_DCA']['tl_calendar_events']['list']['global_operations'] ?? []
 );
 
 /*
