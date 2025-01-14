2f0769262a6fc534f003317df70da93d268f0b35
Janborg <jan-github@luenborg.eu>
2025-01-14 18:21:52 +0100

    bugfix,  one $ too much

--- src/Command/H4aSpielplanCommand.php
+++ src/Command/H4aSpielplanCommand.php
@@ -89,7 +89,7 @@ class H4aSpielplanCommand extends Command
 
         if (null !== $ligaID) {
             $arrResultTabelle = $this->h4aApiHelper
-                ->setLvIDNext($$ligaID)
+                ->setLvIDNext($ligaID)
                 ->getTabelleForClassID()
             ;
         } else {
