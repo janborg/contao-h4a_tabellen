b2bf4d375db5d0ae33933707f324334c7b968e2a
Janborg <jan-github@luenborg.eu>
2025-02-03 07:35:34 +0100

    add TeamsCrawler and VerbandsCrawler

---
+++ src/Crawler/TeamsCrawler.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_gamestats.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+namespace Janborg\H4aTabellen\Crawler;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+
+class TeamsCrawler
+{
+    private string $baseUrl = 'https://www.handball.net';
+
+    private string $clubID;
+
+    private string $season;
+
+    private string $verbandName;
+
+    /**
+     * @var array<mixed>
+     */
+    private array $teams;
+
+    private Crawler $crawler;
+
+    public function setClubID(string $clubID): void
+    {
+        $this->clubID = $clubID;
+    }
+
+    public function setVerbandName(string $verbandName): void
+    {
+        $this->verbandName = urlencode(strtolower($verbandName));
+    }
+
+    public function setSeason(string $season): void
+    {
+        $this->season = $season;
+    }
+
+    /**
+     * creates a Crawler and crawls all the teams for a club. Relevant inputs must be
+     * set upfront.
+     *
+     * @return array<mixed>
+     */
+    public function getAllTeams(): array
+    {
+        $this->getCrawler();
+
+        $this->crawlTeams();
+
+        return $this->teams;
+    }
+
+    private function getClubUrl(): string
+    {
+        $cluburl = $this->baseUrl.'/vereine/handball4all.'.$this->verbandName.'.'.$this->clubID;
+
+        if (isset($this->season)) {
+            $cluburl .= '?season='.$this->season;
+        }
+
+        return $cluburl;
+    }
+
+    private function getCrawler(): void
+    {
+        $url = $this->getClubUrl();
+
+        $httpClient = HttpClient::create();
+
+        $response = $httpClient->request('GET', $url);
+
+        $html = $response->getContent();
+
+        $this->crawler = new Crawler($html);
+    }
+
+    private function crawlTeams(): void
+    {
+        $divMain = $this->crawler->filterXPath('//body//main');
+
+        $arrTeams = [];
+
+        $divMain->filterXPath('//a[contains(@class, "list-item")]')->each(
+            static function (Crawler $node, $i) use (&$arrTeams): void {
+                $arrTeams[$i]['teamUrl'] = $node->attr('href');
+
+                $arrTeams[$i]['teamName'] = $node->filterXPath('//div[contains(@class, "list-item-title")]')->text();
+            },
+        );
+
+        // add teamID to array
+        foreach ($arrTeams as &$team) {
+            $team['teamID'] = $this->extractTeamID($team['teamUrl']);
+        }
+
+        $this->teams = $arrTeams;
+    }
+
+    private function extractTeamID(string $url): string
+    {
+        preg_match('/mannschaften\/\w+\.\w+\.(\d+)\//', $url, $matches);
+
+        return $matches[1] ?? '';
+    }
+}
