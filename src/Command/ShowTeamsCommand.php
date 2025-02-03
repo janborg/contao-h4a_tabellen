19557b4f26f35840e75d8f78fb8829753685a409
Janborg <jan-github@luenborg.eu>
2025-02-03 07:35:58 +0100

    add Show commands for teams and verband

---
+++ src/Command/ShowTeamsCommand.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_tabellen.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+namespace Janborg\H4aTabellen\Command;
+
+use Symfony\Component\Console\Helper\Table;
+use Janborg\H4aTabellen\Crawler\TeamsCrawler;
+use Symfony\Component\Console\Command\Command;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Janborg\H4aTabellen\Crawler\VerbandsCrawler;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+/**
+ * Class UpdateLineupCommand.
+ *
+ * @property SymfonyStyle $io
+ * @property int          $statusCode
+ */
+class ShowTeamsCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'h4a:show:teams';
+
+    /**
+     * @var string
+     */
+    protected static $defaultDescription = 'Show teams of a given club from handballnet';
+
+    public function __construct(
+        private ContaoFramework $framework,
+        private TeamsCrawler $teamsCrawler,
+        private VerbandsCrawler $verbandsCrawler,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp('This command allows you to show all Teams for a club from handball.net.')
+            ->addOption('clubID', null, InputOption::VALUE_REQUIRED , 'clubID from handball.net')
+            ->addOption('verband', null, InputOption::VALUE_REQUIRED , 'verband from handball.net')
+            ->addOption('season', null, InputOption::VALUE_REQUIRED , 'season from handball.net')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->framework->initialize();
+
+        $io = new SymfonyStyle($input, $output);
+
+        $clubID = $input->getOption('clubID');
+
+        if (!$clubID) {
+            $io->error('Bitte die Club ID (--clubID) angeben');
+            return Command::FAILURE;
+        }
+
+        $this->teamsCrawler->setClubID($clubID);
+
+        $verband = $input->getOption('verband');
+
+        if (!$verband) {    
+            $verbaende = $this->verbandsCrawler->getAllVerbaende();
+
+            $verbaendeShorts = array_map(function ($verband) {
+                return $verband['verbandShortName'];
+            }, $verbaende);
+
+            $question = new ChoiceQuestion(
+                'Bitte wählen Sie den Verband aus, in dem der verein spielt:',
+                $verbaendeShorts,
+                null
+            );
+
+            $question->setErrorMessage('Verband %s ist ungültig.');
+
+            $verband = $io->askQuestion($question);
+        }
+
+        $this->teamsCrawler->setVerbandName($verband);        
+
+        $season = $input->getOption('season');
+
+        if ($season) {
+            $this->teamsCrawler->setSeason($season);
+        }
+
+        try {
+            $teams = $this->teamsCrawler->getAllTeams();
+        } catch (\Exception $e) {
+            $io->error($e->getMessage());
+
+            return Command::FAILURE;
+        }
+        
+        $io->info('Teams for ClubID: '.$clubID.' (Verband: '.$verband.'in der Saison: '.$season.')');
+
+        $tablehome = new Table($output);
+        $tablehome->setHeaders(['TeamUrl', 'Team', 'TeamID']);
+        $tablehome->setRows($teams);
+        $tablehome->render();
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Command/ShowVerbaendeCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_tabellen.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+namespace Janborg\H4aTabellen\Command;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Command\Command;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Janborg\H4aTabellen\Crawler\VerbandsCrawler;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class UpdateLineupCommand.
+ *
+ * @property SymfonyStyle $io
+ * @property int          $statusCode
+ */
+class ShowVerbaendeCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'h4a:show:verbaende';
+
+    /**
+     * @var string
+     */
+    protected static $defaultDescription = 'Show all verbaende from handballnet';
+
+    public function __construct(
+        private ContaoFramework $framework,
+        private VerbandsCrawler $verbandsCrawler,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp('This command allows you to show all Verbaende from handball.net.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->framework->initialize();
+
+        $verbaende = $this->verbandsCrawler->getAllVerbaende();
+
+        $table = new Table($output);
+        $table->setHeaders(['Url', 'Name', 'ShortName']);
+        $table->setRows($verbaende);
+        $table->render();
+
+        return Command::SUCCESS;
+    }
+}
