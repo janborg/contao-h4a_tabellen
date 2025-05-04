ca77baf445e86ca34a7b9f871bc2129890708702
janborg <jan+github@luenborg.eu>
2025-05-04 16:42:17 +0200

    fix: stop logging not found reportNo

--- src/Cron/UpdateH4aReportNoCron.php
+++ src/Cron/UpdateH4aReportNoCron.php
@@ -75,11 +75,7 @@ class UpdateH4aReportNoCron
                 $this->systemLogger
                     ->info('Report Nr. '.$objEvent->sGID.' für Spiel '.$objEvent->title.' ('.$objEvent->gGameID.') über Handball4all gespeichert')
                 ;
-            } else {
-                $this->systemLogger
-                    ->info('Report Nr. für Spiel '.$objEvent->title.' ('.$objEvent->gGameID.') konnte nicht ermittelt werden')
-                ;
-            }
+            } 
         }
     }
 }
