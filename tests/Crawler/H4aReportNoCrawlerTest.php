d7cc45259bd6bf3e950cba759c0824f77c45a1ee
Jan <jan+github@luenborg.eu>
2025-05-13 23:05:27 +0200

    add Test for H4aReportNoCrawler (#80)

---
+++ tests/Crawler/H4aReportNoCrawlerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use Janborg\H4aTabellen\Crawler\H4aReportNoCrawler;
+use PHPUnit\Framework\TestCase;
+
+class H4aReportNoCrawlerTest extends TestCase
+{
+    /**
+     * @return array<int, array<int, string>>
+     */
+    public static function gamesProvider(): iterable
+    {
+        return [
+            ['handball4all', 'wuerttemberg', '126171', 'm-bol_hf','7762911', '2651371'],
+            ['handball4all', 'baden', '118076', 'f-olb_bhv','7448846', '2985786'],
+        ];
+    }
+
+    /**
+     * @dataProvider gamesProvider
+     *
+     * @param string $provider
+     * @param string $verbandName
+     * @param string $classID
+     * @param string $classShortName
+     * @param string $gGameID
+     * @param string $sGID
+     */
+    public function testGetsReportNo($provider, $verbandName, $classID, $classShortName, $gGameID, $sGID): void
+    {
+        $crawler = new H4aReportNoCrawler();
+        $crawler->setProvider($provider);
+        $crawler->setVerbandName($verbandName);
+        $crawler->setClassID($classID);
+        $crawler->setClassShortName($classShortName);
+        $crawler->setgGameID($gGameID);
+
+        $crawler->crawlReportNo();
+        $reportNo = $crawler->getSGid();
+
+        $this->assertIsString($reportNo);
+        $this->assertNotEmpty($reportNo);
+        $this->assertEquals($sGID, $reportNo);
+    }
+}
