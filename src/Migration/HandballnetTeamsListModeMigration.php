634c9607df07a0affc0db356204f9a276e7a9a0d
janborg <jan+github@luenborg.eu>
2025-06-09 21:09:19 +0200

    add column PID in migration directly

--- src/Migration/HandballnetTeamsListModeMigration.php
+++ src/Migration/HandballnetTeamsListModeMigration.php
@@ -34,23 +34,16 @@ class HandballnetTeamsListModeMigration extends AbstractMigration
         $columns = $schemaManager->listTableColumns('tl_hn_teams');
 
         // If the field pid does not exist in tl_hn_teams we should do nothing
-        if (!isset($columns['pid'])) {
-            return false;
-        }
-
-        // find all tl_hn_teams with empty pid
-        $teamsWithoutPid = $this->connection->executeQuery('
-                SELECT * FROM tl_hn_teams WHERE pid = 0
-            ')
-            ->fetchAllAssociative()
-        ;
-
-        return !empty($teamsWithoutPid) ? true : false;
+        return (!isset($columns['pid'])) ? true : false;
     }
 
     public function run(): MigrationResult
     {
         $this->framework->initialize();
+
+        $this->connection->executeQuery('
+           ALTER TABLE tl_hn_teams ADD COLUMN pid INT(10) NOT NULL DEFAULT 0
+        ');  
 
         // foreach tl_h4a_season, find all tl_hn_teams where hn_season equals saison
         $seasons = H4aSeasonModel::findAll();
