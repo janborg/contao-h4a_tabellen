236ac1e09735db6f3c074f7f34a59cab6e2893df
janborg <jan+github@luenborg.eu>
2025-07-11 14:12:13 +0200

    Fix 500 caused by empty body from api.h4a.mobi Fixes #92

--- src/Helper/H4aApiHelper.php
+++ src/Helper/H4aApiHelper.php
@@ -225,10 +225,24 @@ final class H4aApiHelper
                 $item->expiresAfter($this->h4aCacheTtl);
 
                 try {
-                    return $this->httpClient->request(
+                    $response = $this->httpClient->request(
                         'GET',
                         $this->request_url,
-                    )->toArray();
+                    );
+
+                    if (empty($response->getContent())) {
+                        // Handle the empty response body
+                        $item->expiresAfter(0);
+                        $response = [
+                            0 => [
+                                'dataList' => '',
+                            ],
+                        ];
+                        return $response;
+
+                    } else {
+                        return $response->toArray();
+                    }
                 } catch (TransportExceptionInterface|HttpExceptionInterface $e) {
                     $this->contaoLogger->error(\sprintf('Unable to get data from "%s": %s', $this->request_url, $e->getMessage()));
                     $item->expiresAfter(0);
