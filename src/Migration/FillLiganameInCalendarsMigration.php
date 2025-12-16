20f990916694189605157a2df4be8acaee0bd7bb
janborg <jan+github@luenborg.eu>
2025-12-16 14:41:10 +0100

    Fix Bug in Migration

--- src/Migration/FillLiganameInCalendarsMigration.php
+++ src/Migration/FillLiganameInCalendarsMigration.php
@@ -12,9 +12,10 @@ declare(strict_types=1);
 
 namespace Janborg\H4aTabellen\Migration;
 
-use Contao\CoreBundle\Migration\AbstractMigration;
-use Contao\CoreBundle\Migration\MigrationResult;
 use Doctrine\DBAL\Connection;
+use Contao\CoreBundle\Migration\MigrationResult;
+use Contao\CoreBundle\Migration\AbstractMigration;
+use Janborg\H4aTabellen\Model\HandballnetTeamsModel;
 
 class FillLiganameInCalendarsMigration extends AbstractMigration
 {
@@ -87,8 +88,13 @@ class FillLiganameInCalendarsMigration extends AbstractMigration
                 if (
                     empty($season['liga_name'])
                 ) {
+                    //get teamModel
+                    $team = HandballnetTeamsModel::findby(
+                    ['team_id=?'],
+                    [$season['h4a_team']],
+                );
                     // Setze liga_name
-                    $seasons[$key]['liga_name'] = $season['liga_name'];
+                    $seasons[$key]['liga_name'] = $team->liga_name;
                     $needsUpdate = true;
                 }
             }
