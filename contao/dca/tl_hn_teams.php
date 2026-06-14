0a6c2f4b8ce08de53b7f0c795c2c4454cb0899c7
janborg <jan+github@luenborg.eu>
2026-06-14 20:32:12 +0200

    Fix Backlink for tl_hn_teams

--- contao/dca/tl_hn_teams.php
+++ contao/dca/tl_hn_teams.php
@@ -26,7 +26,6 @@ $GLOBALS['TL_DCA']['tl_hn_teams'] = [
                 'id' => 'primary',
             ],
         ],
-        'backlink' => 'do=handballnet_teams'
     ],
     'list' => [
         'sorting' => [
