be5c64769db8d3aebe2f7ae7451420e60cde7602
janborg <jan+github@luenborg.eu>
2026-08-01 16:54:05 +0200

    fix commands

--- src/Command/H4aUpdateEventsCommand.php
+++ src/Command/H4aUpdateEventsCommand.php
@@ -20,10 +20,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * @property SymfonyStyle $io
- * @property int          $statusCode
- */
 #[AsCommand(
     name: 'handballnet:update:events',
     description: 'Update all Events from h4a',
@@ -39,7 +35,7 @@ class H4aUpdateEventsCommand extends Command
 
     protected function configure(): void
     {
-        $this->setHelp('This command allows youto update all events that are linked to handballnet');
+        $this->setHelp('This command allows you to update all events that are linked to handballnet');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -48,9 +44,9 @@ class H4aUpdateEventsCommand extends Command
 
         $output->writeln('Suche Kalender mit H4a-Events:');
 
-        $objCalendars = CalendarModel::findby(
-            ['tl_calendar.h4a_imported=?'],
-            ['1'],
+        $objCalendars = CalendarModel::findBy(
+            ['tl_calendar.handballnet_imported=?'],
+            [true],
         );
 
         if (null === $objCalendars) {
@@ -65,7 +61,7 @@ class H4aUpdateEventsCommand extends Command
         }
 
         $output->writeln([
-            'Es wurden '.\count($objCalendars).' Kalender zum Update über Handballnet gefunden gefunden.',
+            'Es wurden '.\count($objCalendars).' Kalender zum Update über Handballnet gefunden.',
             'Versuche nun die Updates der Kalender durchzuführen',
             '==========================================================',
             '',
@@ -80,12 +76,19 @@ class H4aUpdateEventsCommand extends Command
             ]);
             $output->writeln('Starte Update...');
 
-            $this->h4aEventAutomator->syncCalendars($objCalendar, false);
+            try {
+                $this->h4aEventAutomator->syncCalendars($objCalendar, false);
 
-            $output->writeln([
-                '<info>Update des Kalenders über Handballnet durchgeführt.</info>',
-                '',
-            ]);
+                $output->writeln([
+                    '<info>Update des Kalenders über Handballnet durchgeführt.</info>',
+                    '',
+                ]);
+            } catch (\Throwable $e) {
+                $output->writeln([
+                    '<error>Update des Kalenders fehlgeschlagen: '.$e->getMessage().'</error>',
+                    '',
+                ]);
+            }
         }
 
         return Command::SUCCESS;
