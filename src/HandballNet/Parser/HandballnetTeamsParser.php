a74e6ad8634d4800f36840727c4e0c3c336c6a4d
janborg <jan+github@luenborg.eu>
2026-09-03 05:56:04 +0200

    run ecs --fix

--- src/HandballNet/Parser/HandballnetTeamsParser.php
+++ src/HandballNet/Parser/HandballnetTeamsParser.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of contao-h4a_tabellen.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
 namespace Janborg\H4aTabellen\HandballNet\Parser;
 
 use Janborg\H4aTabellen\HandballNet\DataTransferObject\TeamDto;
